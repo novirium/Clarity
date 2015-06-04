bc7169fff2b4215186a3ee94271e08fdffa73370
twilsonau <t.wilson.au@gmail.com>
2015-06-04 13:55:23 +0800

    Added basic journal parsing into local datastructure

--- NOTES.md
+++ NOTES.md
@@ -34,3 +34,24 @@ Priority module/plugin then uses this dataset. List of tasks is filled out, for 
 
 
 
+Parse journal textfile and populate local datastructure in memory.
+Journal - a dict or hashmap of entries, and a dict of tag instance lists
+||
+|+Entries - a time ordered list of entries.
+|  |
+|  +Entry - class instance containing the entry time/id and the contained text. Contains a character index ordered list of tag instances
+|  |
+|  +Tag - class instance describing a tag and field within and entry
++TagLists - a dict containg all tag names in the journal. Each name has an assosiated list of instances
+ |
+ +TagList - a time ordered list of tag instaces. Refers directly to tag instances in the Entry class instances
+
+Need to determine the structure of this data. Potential lookup operations:
+Priority module:
+Build a list of tasks with associated dependancy rules (required to generate the priority field over time). Start with list of task tags. Each task tag needs a list of entries it appears in. Need to build a local list for each task containing the constraints and modifiers that apply to it (and their time). In each associated entry, only start looking at tags after the given task tag instance, and stop when another task tag is reached or the entry ends.
+Trying to make the data structure plugin agnostic, so do not want to have to store the relevant task tag in the record for each modifier tag, as this would require definitions from the priority plugin.
+Could store list of tags in the entry, in character order, or just scan through the entry text. Tags in their instance list could then refer either to an entry and the character index, or directly to the tag instance in the entry.
+
+
+
+
--- src/parser.py
+++ src/parser.py
@@ -1,9 +1,93 @@
 #!/usr/bin/python
 
 import sys
+from optparse import OptionParser
 
-if len(sys.argv) == 1:
-    print "Parsing ", str(sys.argv)
-else:
-    print "No journal file given"
+class Journal:
+    def __init__(self):
+        self.entries = []#time ordered list of entries
+        self.tagdict = {}#dict of tag instance lists, using tag name as keyword
+
+class Entry:
+    def parse(self):
+        tagindex=self.text.find('<')
+        while (tagindex>-1):
+            tagend=self.text.find('>',tagindex+1)
+            tempname,_,tempfield=self.text[tagindex+1:tagend].partition(':')
+            #print "Name:",tempname,"Field:",tempfield
+            self.tags.append(Tag(tempname,tempfield))
+            tagindex=self.text.find('<',tagindex+1)
+            
+    def __init__(self,text):
+        self.time=0 # Unix time identifier
+        self.text=text # text of entry
+        self.tags=[] # character index order list of tags in entry
+        self.parse()
+        self.validentry=False
         
+        if len(self.tags)>0:
+            if (self.tags[0].name=="Entry"):
+                if len(self.tags[0].field)>1:
+                    self.validentry=True
+        
+class Tag:
+    def __init__(self,name,field):
+        self.name=name # name of tag
+        self.field=field # field text
+        
+        
+        
+        
+journal=Journal()
+
+
+        
+def process_new_entry(newentry):
+    if newentry.validentry:
+        journal.entries.append(newentry)
+    
+        for newtag in newentry.tags:
+            if journal.tagdict.has_key(newtag.name):
+                journal.tagdict[newtag.name].append(newtag)
+            else:
+                journal.tagdict[newtag.name]=[newtag]
+                
+            
+def main(argv):
+    
+    optparser = OptionParser()
+    optparser.add_option("-j", "--journalfile", dest="journalfilename",
+            help="Journal file to parse")
+    optparser.add_option("-o", "--outputfile", dest="outputfilename",
+            help="Output file to write tag dictionary into")
+            
+    (options, args) = optparser.parse_args();
+    
+
+    
+    
+    with open(options.journalfilename) as jfile:
+        entryindex=-1
+        tempstring=" "
+        for jline in jfile:
+            tempstring+=jline
+            entryindex=tempstring.find("<Entry:",1)
+            while (entryindex>-1):
+                newentry=Entry(tempstring[:entryindex])
+                process_new_entry(newentry)
+                
+                            
+                tempstring=tempstring[entryindex:]
+                entryindex=tempstring.find("<Entry:",1)
+
+        newentry=Entry(tempstring)
+        process_new_entry(newentry)
+                
+    print "\n".join(map(lambda e: str(map(lambda t: t.name, e.tags)),journal.entries))
+    print "\n"
+    print "\n".join(map(lambda (k,tl): k+":"+str(map(lambda tag:tag.field,tl)),journal.tagdict.items()))
+
+    
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
