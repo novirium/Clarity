94a43f490aa872f93ed5af7e7be04dd95ce97c6c
twilsonau <t.wilson.au@gmail.com>
2015-06-06 01:58:57 +0800

    Added command options to show parsed tag tables (-t), and add an entry (-a) with a journal file rewrite

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys,jdata
+import sys,jdata,time
 from optparse import OptionParser
 
         
@@ -18,40 +18,67 @@ def process_new_entry(newentry):
             else:
                 journal.tagdict[newtag.name]=[newtag]
                 
+
+def write_journal(filename):
+    with open(filename, "w+") as jfile:
+        for entry in journal.entries:
+            jfile.write(entry.text)
+                
             
 def main(argv):
     
     optparser = OptionParser()
     optparser.add_option("-j", "--journalfile", dest="journalfilename",
             help="Journal file to parse")
-    optparser.add_option("-o", "--outputfile", dest="outputfilename",
-            help="Output file to write tag dictionary into")
+    optparser.add_option("-a", "--addentry", dest="addentry", default="",
+            help="Add an entry and save it back to the journal file. Automatically adds <Entry> tag.")
+    optparser.add_option("-t", "--tagtables", help="Show the parsed tagtables", action="store_true", dest="tagtables", default=False)
             
     (options, args) = optparser.parse_args();
     
 
     
-    
-    with open(options.journalfilename) as jfile:
-        entryindex=-1
-        tempstring=" "
-        for jline in jfile:
-            tempstring+=jline
-            entryindex=tempstring.find("<Entry:",1)
-            while (entryindex>-1):
-                newentry=jdata.Entry(tempstring[:entryindex])
-                process_new_entry(newentry)
-                
-                            
-                tempstring=tempstring[entryindex:]
+    try:
+        with open(options.journalfilename) as jfile:
+            entryindex=-1
+            tempstring=" "
+            for jline in jfile:
+                tempstring+=jline
                 entryindex=tempstring.find("<Entry:",1)
-
-        newentry=jdata.Entry(tempstring)
-        process_new_entry(newentry)
-                
-    print "\n".join(map(lambda e: str(map(lambda t: t.name, e.tags)),journal.entries))
-    print "\n"
-    print "\n".join(map(lambda (k,tl): k+":"+str(map(lambda tag:tag.field,tl)),journal.tagdict.items()))
+                while (entryindex>-1):
+                    newentry=jdata.Entry(tempstring[:entryindex])
+                    process_new_entry(newentry)
+                    
+                                
+                    tempstring=tempstring[entryindex:]
+                    entryindex=tempstring.find("<Entry:",1)
+    
+            newentry=jdata.Entry(tempstring)
+            process_new_entry(newentry)
+    except IOError:
+         print "Could not open journal file"
+     
+    if options.addentry:
+        entrytime=int(time.time())*1000
+        timemod=0
+        while(journal.isentrytimeunique(entrytime+timemod)==False) and (timemod<1000):
+            timemod=timemod+1
+            
+        if (timemod>=1000) :
+            print "Could not find valid timestamp"
+        else:
+            entrytime=entrytime+timemod
+            newentry=jdata.Entry("\n<Entry:"+str(entrytime)+">"+options.addentry+"\n")
+            process_new_entry(newentry)
+            if newentry.validentry:
+                print "\nAdded:",newentry.text
+            write_journal(options.journalfilename)
+            print "Updated journal"
+            
+    if options.tagtables:
+        print "\n".join(map(lambda e: str(map(lambda t: t.name, e.tags)),journal.entries))
+        print "\n"
+        print "\n".join(map(lambda (k,tl): k+":"+str(map(lambda tag:tag.field,tl)),journal.tagdict.items()))
 
     
     
